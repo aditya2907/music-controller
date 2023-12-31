3bd361c47355cb854d508e43749c0b0c93b3661e
Aditya Suryawanshi <suryawanshiaditya29@gmail.com>
2024-01-01 05:06:30 +0530

    setup route for pages

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,17 +1,19 @@
-import React , {Component} from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
-
+import HomePage from "./HomePage";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
-
     render() {
-        return <h1>Testing Code</h1>;
+        return (
+            <div>
+                <HomePage />
+            </div>
+        )
     }
-
 }
 
-const appDiv  = document.getElementById("app");
-render(<App/>, appDiv);
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
