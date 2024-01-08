d50662a2044f3b3dc8b04e1074b15716aa7227d6
Aditya Suryawanshi <suryawanshiaditya29@gmail.com>
2024-01-08 10:51:30 +0530

    setup join-room

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -40,15 +40,19 @@ export default class CreateRoomPage extends Component {
 	handleRoomButtonPressed() {
 		const requestOptions = {
 			method: "POST",
-			headers: { "Content-Type": "application/json" },
+			// headers: { "Content-Type": "application/json" },
+			headers:{ accept: 'application/json'},
 			body: JSON.stringify({
 				votes_to_skip: this.state.votesToSkip,
 				guest_can_pause: this.state.guestCanPause,
 			}),
 		};
-		fetch("http://localhost:3002", requestOptions)
+		fetch("/api/create-room", requestOptions)
 			.then((response) => response.json())
-			.then((data) => this.props.history.push('/room/' + data.code));
+			.then((data) => this.props.history.push("/room/" + data.code))
+			// // .then((response) => console.log(response.json()))
+			// .then((data) => console.log(data))
+
 	}
 
 	render() {
