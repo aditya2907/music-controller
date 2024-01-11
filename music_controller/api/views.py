f40f059a549aab291ac46e761c40355dc842afc3
Aditya Suryawanshi <suryawanshiaditya29@gmail.com>
2024-01-12 04:42:45 +0530

    styled homepage and redirected correct path for home

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework import generics, status
 from .serializers import RoomSerializer, CreateRoomSerializer
@@ -74,8 +75,18 @@ class CreateRoomView(APIView):
                 room = Room(host=host, guest_can_pause=guest_can_pause,
                             votes_to_skip=votes_to_skip)
                 room.save()
-                print(room)
+                # print(room)
                 self.request.session['room_code'] = room.code
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
             
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        data = {
+            'code' : self.request.session['room_code']
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
